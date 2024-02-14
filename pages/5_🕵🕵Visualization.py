7cfcd66ca51f9b5fe84305acf972ca6499c67006
nadarktong <neangez@gmail.com>
2024-02-14 11:40:51 +0700

    Update 5_🕵🕵Visualization.py

--- pages/5_🕵🕵Visualization.py
+++ pages/5_🕵🕵Visualization.py
@@ -10,15 +10,6 @@ data = pd.DataFrame({
 })
 
 import matplotlib.pyplot as plt
-labels = 'Men', 'Women'
-sizes = [NumM[1],NumF[1]]
-explode = (0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
-
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-st.pyplot(fig1)
-
 # แสดงชื่อกราฟ
 st.title("ข้อมูลประเทศ")
 
@@ -29,11 +20,17 @@ chart_type = st.selectbox("เลือกประเภทกราฟ", ["bar
 if chart_type == "bar":
     st.bar_chart(data, x="Country", y="Population")
 elif chart_type == "pie":
-    st.pie_chart(data, values="Population", labels="Country")
+    labels = "ไทย", "อเมริกา", "จีน", "ญี่ปุ่น"
+    sizes =  [70000000, 330000000, 1400000000, 125000000]
+    explode = (0, 0.1,0,0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+    st.pyplot(fig1)
 elif chart_type == "line":
     st.line_chart(data, x="Country", y="Population")
-elif chart_type == "scatter":
-    st.scatter_plot(data, x="Country", y="Population")
+
 
 # แสดงข้อมูล GDP
 st.write("ข้อมูล GDP")

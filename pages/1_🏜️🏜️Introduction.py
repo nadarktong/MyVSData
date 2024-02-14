548f440e90ff815f289a4144d5f4b784c936c35a
nadarktong <neangez@gmail.com>
2024-02-14 10:56:16 +0700

    Update 1_🏜️🏜️Introduction.py

--- pages/1_🏜️🏜️Introduction.py
+++ pages/1_🏜️🏜️Introduction.py
@@ -4,21 +4,21 @@ import requests
 import streamlit as st
 
 st.set_page_config(
-    page_title="Kairung Datascience Project",
+    page_title="Data visualization Project",
     page_icon= ":bar_chart:",
 )
 st.sidebar.success("เลือกรายการด้านบน.")
 
-st.header("# 🍀🍀🍀 การพยากรณ์ข้อมูล....! 👋  🧑🏽‍🏫 ")
-st.subheader("### 1.หลักการและเหตุผล")
+st.header("# 🍀🍀🍀 การทำจินตทัศน์ข้อมูล....! 👋  🧑🏽‍🏫 ")
+st.subheader("1.หลักการและเหตุผล")
 st.info("""
         xxxxxxxxxxxxxxxxxxxxxxx <br>
         xxxxxxxxxxxxxxxxxxxxxxx
         xxxxxxxxxxxxxxxxxxxxxxx
         xxxxxxxxxxxxxxxxxxxxxxx
         """)
-st.subheader("### 2.วัตถุประสงค์")
+st.subheader("2.วัตถุประสงค์")
 st.info("""
-        เพื่อศึกษาเทคนิคสำหรับการพยากรณ์ข้อมูลในงานเหมืองข้อมูล
+        เพื่อนำเสนอข้อมูลในรูปแบบที่เข้าใจได้ง่ายด้วยวิธีการจินตทัศน์ข้อมูล
         """)
 st.balloons()

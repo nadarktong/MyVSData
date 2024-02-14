d8ff5fdb268d949dccdc2a4c62d2ed6e693db2f0
nadarktong <neangez@gmail.com>
2024-02-14 11:28:37 +0700

    Update 5_🕵🕵Visualization.py

--- pages/5_🕵🕵Visualization.py
+++ pages/5_🕵🕵Visualization.py
@@ -4,16 +4,26 @@ import pandas as pd
 # ตัวอย่างข้อมูล
 data = pd.DataFrame({
     "Country": ["ไทย", "อเมริกา", "จีน", "ญี่ปุ่น"],
-    "Population": [70000000, 330000000, 1400000000, 125000000]
+    "Population": [70000000, 330000000, 1400000000, 125000000],
+    "GDP": [500000000000, 25000000000000, 15000000000000, 5000000000000]
 })
 
 # แสดงชื่อกราฟ
-st.title("ประชากรในบางประเทศ")
+st.title("ข้อมูลประเทศ")
 
-# แสดง bar chart
-st.bar_chart(data, x="Country", y="Population")
+# เลือกประเภทกราฟ
+chart_type = st.selectbox("เลือกประเภทกราฟ", ["bar", "pie", "line", "scatter"])
 
-st.line_chart(data, x="Country", y="Population")
-st.pie_chart(data, values="Population", labels="Country")
-st.bar_chart(data, x="Country", y="Population")
+# แสดงกราฟ
+if chart_type == "bar":
+    st.bar_chart(data, x="Country", y="Population")
+elif chart_type == "pie":
+    st.pie_chart(data, values="Population", labels="Country")
+elif chart_type == "line":
+    st.line_chart(data, x="Country", y="Population")
+elif chart_type == "scatter":
+    st.scatter_plot(data, x="Country", y="Population")
 
+# แสดงข้อมูล GDP
+st.write("ข้อมูล GDP")
+st.table(data[["Country", "GDP"]])

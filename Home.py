70ba75776c177d12a7c0f06b97b1f708b25484d2
nadarktong <neangez@gmail.com>
2024-02-14 10:44:22 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,7 +12,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/425c8e43-a5bb-4052-aa10-4730e06b1437/m8vsAHh86O.json"
+lottie_url_hello = "https://lottie.host/fcd01209-0ad6-4615-8477-c2d5f76624dd/fDSfJEABkm.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 
 st.header("การพยากรณ์ข้อมูล....ด้วยเทคนิค Linear Regression")
